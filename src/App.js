021cbd1122506771422a151abca5537972afb5dd
dexxlabs <ranbeersingh0520@gmail.com>
2025-05-18 14:34:04 +0530

    Update : Added Bootsplash (Splash Screen)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import fonts from './data/fonts';
+import {color} from './data/color';
+import BootSplash from "react-native-bootsplash";
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+    });
+  }, []);
+
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: '#141414',
+        backgroundColor: color.background,
         paddingTop: StatusBar.currentHeight + 12,
         paddingHorizontal: 12,
+        alignItems:'center'
       }}>
       <Text style={{color: '#fff', fontFamily: fonts.xli, fontSize: 16}}>
         AppDev
       </Text>
+      <View
+        style={{
+          marginTop:10,
+          height: 50,
+          width:200,
+          backgroundColor: color.secondary,
+          borderRadius: 15,
+        }}></View>
+        <View
+        style={{
+          marginTop:10,
+          height: 50,
+          width:200,
+          backgroundColor: color.primary,
+          borderRadius: 15,
+        }}></View>
     </View>
   );
 };
---
+++ src/data/color.js
@@ -0,0 +1,5 @@
+export const color = {
+  background: '#0A0A0A',
+  primary: '#22b455',
+  secondary: '#1A1A1A',
+};
